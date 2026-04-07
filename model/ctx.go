e4c9a1198abc6c5741d85df7c0914fa2c785cfdf
bincooo <bingco.zn@gmail.com>
2026-04-07 11:43:09 +0800

    feat: xml tag 解析工具

--- model/ctx.go
+++ model/ctx.go
@@ -57,6 +57,10 @@ func (ctx *Ctx) GetCompletion() *Completion {
 	return JustValue[string, *Completion](ctx.Record, "completion")
 }
 
+func (ctx *Ctx) GetGeneration() *Generation {
+	return JustValue[string, *Generation](ctx.Record, "generation")
+}
+
 func (ctx *Ctx) StreamWriter(accept chan *ChunkBodies, unix int64) {
 	ctx.ctx.Set("content-type", "text/event-stream")
 	ctx.ctx.Set("cache-control", "no-cache")
@@ -81,22 +85,6 @@ func (ctx *Ctx) StreamWriter(accept chan *ChunkBodies, unix int64) {
 			}
 		}
 	})
-
-}
-
-func (ctx *Ctx) streamWriter(yield func(w func(*ChunkBodies) error), unix int64) {
-	ctx.ctx.Set("content-type", "text/event-stream")
-	ctx.ctx.Set("cache-control", "no-cache")
-	ctx.ctx.Set("x-accel-buffering", "no")
-	ctx.ctx.Set("x-accept-encoding", "gzip, deflate, br")
-	ctx.ctx.Set("connection", "keep-alive")
-	ctx.ctx.Set("transfer-encoding", "chunked")
-	_ = ctx.ctx.SendStreamWriter(func(w *bufio.Writer) {
-		yield(func(bodies *ChunkBodies) error {
-			return write(w, bodies, unix)
-		})
-	})
-	return
 }
 
 func (ctx *Ctx) Writer(msg interface{}) error {
