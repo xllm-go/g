23325f20cabdc151da97b295214ee8959c32a273
bincooo <bingco.zn@gmail.com>
2026-04-22 01:15:44 +0800

    feat: 绘图添加重试机制

--- builder.go
+++ builder.go
@@ -1,6 +1,9 @@
 package g
 
 import (
+	"time"
+
+	"github.com/xllm-go/g/logger"
 	"github.com/xllm-go/g/model"
 
 	v1 "github.com/xllm-go/g/internal/v1"
@@ -23,11 +26,47 @@ func (receiver *builder) Relay(yield func(ctx *model.Ctx) error) {
 }
 
 // 文生图
-func (receiver *builder) Image(yield func(ctx *model.Ctx) error) {
+func (receiver *builder) Image(yield func(ctx *model.Ctx) error, retry ...int) {
+	if l := len(retry); l > 0 {
+		sleep := 0
+		if l > 1 && retry[1] > 0 {
+			sleep = retry[1]
+		}
+
+		ex := yield
+		yield = func(ctx *model.Ctx) error {
+			return execute(func() error {
+				return ex(ctx)
+			}, retry[0], sleep)
+		}
+	}
 	receiver.yield = yield
 	receiver.build("image")
 }
 
 func (receiver *builder) build(typed string) {
 	v1.Put(typed, receiver.slice, receiver.yield)
+}
+
+func execute(yield func() error, retry int, sleep int) (err error) {
+	if retry < 0 {
+		retry = 0
+	}
+
+label:
+	err = yield()
+	if err == nil {
+		return
+	}
+
+	if retry > 0 {
+		retry--
+		logger.Sugar().Warnf("retry by err: %v", err)
+		if sleep > 0 {
+			time.Sleep(time.Duration(sleep) * time.Second)
+		}
+		goto label
+	}
+
+	return
 }
